dc8e34c485e358e68f98c590a09748bb0014c11f
MBradford <MBradford@Cfms4.com>
2022-09-04 00:39:21 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.40e69cd08f2d0c78d2ed3d4abebd0f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2bd81dc19675a2cec42",
+    "url": "/marvelcrewcreator/static/css/main.78427418.chunk.css"
+  },
+  {
+    "revision": "e2bd81dc19675a2cec42",
+    "url": "/marvelcrewcreator/static/js/main.e2bd81dc.chunk.js"
+  },
+  {
+    "revision": "dbe6079872b7e647bc87",
+    "url": "/marvelcrewcreator/static/js/1.dbe60798.chunk.js"
+  },
+  {
+    "revision": "42358577c85e400d5860",
+    "url": "/marvelcrewcreator/static/js/runtime~main.42358577.js"
+  },
+  {
+    "revision": "e65ca72257aa9beeedee87f081c3483c",
+    "url": "/marvelcrewcreator/static/media/marvel_loader.e65ca722.gif"
+  },
+  {
+    "revision": "abc6e10511c9dea0fadc8d52c9d41f23",
+    "url": "/marvelcrewcreator/index.html"
+  }
+];
